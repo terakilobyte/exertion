f4acec407617ca160a321541804d76bc65fd6fe8
terakilobyte <terakilobyte@gmail.com>
2016-08-06 02:16:17 -0700

    more font dickery

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
       }, {
         test: /\.scss$/,
         loader: extractSass.extract(['css', 'sass'])
+      }, {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
     ]
   },
