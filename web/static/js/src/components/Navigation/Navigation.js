fd622bf4813ce14da8a88a030cd2a135108c2ac6
Nathan Leniz <terakilobyte@gmail.com>
2016-11-24 22:45:25 -0800

    use automatic return in pure function

--- web/static/js/src/components/Navigation/Navigation.js
+++ web/static/js/src/components/Navigation/Navigation.js
@@ -2,24 +2,22 @@ import React from 'react'
 import { Link } from 'react-router'
 import './Navigation.scss'
 
-const Navigation = () => {
-  return (
-    <div className='nav'>
-      <ul className='left'>
-        <li>
-          <h1>
-            <Link to='/'>
-              Exertion
-            </Link>
-          </h1>
-        </li>
-      </ul>
-      <ul className='right'>
-        <li className='hover-effect'>About</li>
-        <li className='hover-effect'>Account</li>
-      </ul>
-    </div>
-  )
-}
+const Navigation = () => (
+  <div className='nav'>
+    <ul className='left'>
+      <li>
+        <h1>
+          <Link to='/'>
+            Exertion
+          </Link>
+        </h1>
+      </li>
+    </ul>
+    <ul className='right'>
+      <li className='hover-effect'>About</li>
+      <li className='hover-effect'>Account</li>
+    </ul>
+  </div>
+)
 
 export default Navigation
