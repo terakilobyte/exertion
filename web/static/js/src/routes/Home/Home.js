46b30ed25f237c738a2f12f382382483600933bc
Nathan Leniz <terakilobyte@gmail.com>
2016-11-27 14:11:57 -0800

    use react-css-modules

--- web/static/js/src/routes/Home/Home.js
+++ web/static/js/src/routes/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
 import PresentationCard from '../../components/PresentationCard'
-import './Home.scss'
+import styles from './Home.scss'
 
 const Home = (props) => (
-  <div className='center-home'>
-    <h3>Trying to make the juice worth the <span className='squeeze'><em>squeeze</em></span></h3>
+  <div id='centerHomeID' className={styles.centerHome}>
+    <h3>Trying to make the juice worth the <span className={styles.squeeze}><em>squeeze</em></span></h3>
     <Link className='presentation-link' to='tictactoe'>
       <PresentationCard content='Tic-Tac-Toe' />
     </Link>
--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,82 @@ const webpackConfig = {
   sassLoader: {
     includePaths: [path.resolve(__dirname) + '/web/static/css']
   },
+  postcss (bundler) {
+    return {
+      default: [
+        cssnano({
+          autoprefixer: {
+            add: true,
+            remove: true,
+            browsers: ['last 2 versions']
+          },
+          discardComments: {
+            removeAll: true
+          },
+          discardUnused: false,
+          mergeIdents: false,
+          reduceIdents: false,
+          safe: true,
+          sourcemap: true
+        }),
+        // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
+        // https://github.com/jonathantneal/postcss-partial-import
+        require('postcss-partial-import')({ addDependencyTo: bundler }),
+        // Allow you to fix url() according to postcss to and/or from options
+        // https://github.com/postcss/postcss-url
+        require('postcss-url')(),
+        // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
+        // https://github.com/postcss/postcss-custom-properties
+        require('postcss-custom-properties')(),
+        // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
+        // https://github.com/postcss/postcss-custom-media
+        require('postcss-custom-media')(),
+        // CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
+        // https://github.com/postcss/postcss-media-minmax
+        require('postcss-media-minmax')(),
+        // W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
+        // https://github.com/postcss/postcss-custom-selectors
+        require('postcss-custom-selectors')(),
+        // W3C calc() function, e.g. div { height: calc(100px - 2em); }
+        // https://github.com/postcss/postcss-calc
+        require('postcss-calc')(),
+        // Allows you to nest one style rule inside another
+        // https://github.com/jonathantneal/postcss-nesting
+        require('postcss-nesting')(),
+        // Unwraps nested rules like how Sass does it
+        // https://github.com/postcss/postcss-nested
+        require('postcss-nested')(),
+        // W3C color() function, e.g. div { background: color(red alpha(90%)); }
+        // https://github.com/postcss/postcss-color-function
+        require('postcss-color-function')(),
+        // Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
+        // https://github.com/iamvdo/pleeease-filters
+        require('pleeease-filters')(),
+        // Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
+        // https://github.com/robwierzbowski/node-pixrem
+        require('pixrem')(),
+        // W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
+        // https://github.com/postcss/postcss-selector-matches
+        require('postcss-selector-matches')(),
+        // Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
+        // https://github.com/postcss/postcss-selector-not
+        require('postcss-selector-not')(),
+        // Postcss flexbox bug fixer
+        // https://github.com/luisrudge/postcss-flexbugs-fixes
+        require('postcss-flexbugs-fixes')(),
+        // Add vendor prefixes to CSS rules using values from caniuse.com
+        // https://github.com/postcss/autoprefixer
+        require('autoprefixer')({
+          browsers: [
+            '>1%',
+            'last 4 versions',
+            'Firefox ESR',
+            'not ie < 9' // React doesn't support IE8 anyway
+          ]
+        })
+      ]
+    }
+  },
   plugins
 }
 
@@ -86,42 +162,43 @@ const webpackConfig = {
 const BASE_CSS_LOADER = 'css?sourceMap&-minimize'
 
 webpackConfig.module.loaders.push({
-  test    : /\.scss$/,
-  exclude : null,
-  loaders : [
+  test: /\.scss$/,
+  exclude: null,
+  loaders: [
     'style',
     BASE_CSS_LOADER,
     'postcss',
-    'sass?sourceMap'
+    'sass'
   ]
 })
 
 webpackConfig.module.loaders.push({
-  test    : /\.css$/,
-  exclude : null,
-  loaders : [
+  test: /\.css$/,
+  exclude: null,
+  loaders: [
     'style',
-    BASE_CSS_LOADER,
+    BASE_CSS_LOADER + '&modules&importLoaders=1&localIdentName=[path]__[name]__[hash:base64:5]',
     'postcss'
   ]
 })
 
-webpackConfig.postcss = [
-  cssnano({
-    autoprefixer : {
-      add      : true,
-      remove   : true,
-      browsers : ['last 2 versions']
-    },
-    discardComments : {
-      removeAll : true
-    },
-    discardUnused : false,
-    mergeIdents   : false,
-    reduceIdents  : false,
-    safe          : true,
-    sourcemap     : true
-  })
-]
+/*
+ * webpackConfig.postcss = [
+ *   cssnano({
+ *     autoprefixer : {
+ *       add      : true,
+ *       remove   : true,
+ *       browsers : ['last 2 versions']
+ *     },
+ *     discardComments : {
+ *       removeAll : true
+ *     },
+ *     discardUnused : false,
+ *     mergeIdents   : false,
+ *     reduceIdents  : false,
+ *     safe          : true,
+ *     sourcemap     : true
+ *   })
+ * ]*/
 
 module.exports = webpackConfig
